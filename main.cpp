d0dbedfe964485c6725119c3e241cc61d8160837
Samuele Pelloni <91503299+samuelepellonii@users.noreply.github.com>
2021-11-30 17:12:29 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
-#include <iostream>
 
-int main(){
-  return 0;
+#include <iostream>
+#include "lib.h"
+
+using namespace std;
+
+int main() {
+    char a=0;
+    cin >> a;
+
+    Vero_o_Falso ( a );
+
+    if ((a >= 65 and a <= 90) or (a >= 97 and a <= 122)) {
+        cout << Trasformazione ( a ) << endl;
+
+    } else {
+        cout << "errore" << endl;
+    }
+
+    return 0;
 }
