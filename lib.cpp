1260d9c7efae8b3de2102d1558ba1d5d8f204a84
Samuele Pelloni <91503299+samuelepellonii@users.noreply.github.com>
2021-11-30 17:09:31 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
 
+#include "lib.h"
+
+bool Vero_o_Falso ( char a ) {
+    if ((a >= 65 and a <= 90) or (a >= 97 and a <= 122)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+char Trasformazione ( char a ){
+    if (a >= 65 and a <= 90) {
+        return a+32;
+    } else if (a >= 97 and a <= 122) {
+        return a-32;
+    }
+}
